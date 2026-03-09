2d0f16f5cd7b742c1a729e8702b803a3f546e893
Andre <andresieiro@hotmail.com>
2026-03-09 16:54:27 -0300

    initial v1

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+};
