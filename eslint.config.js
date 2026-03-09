e4fbb74f52061f736cc76884116cad8cfb51fbb0
Andre <andresieiro@hotmail.com>
2026-03-09 17:43:41 -0300

    initial config lint/prettier and scripts

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '**/*.js'],
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log('🏈 Soccer Session Manager - API');
+console.log('✅ TypeScript configurado com sucesso!');
+
+const testFunction = (name: string): string => {
+  return `Hello, ${name}!`;
+};
+
+console.log(testFunction('Futebol'));
